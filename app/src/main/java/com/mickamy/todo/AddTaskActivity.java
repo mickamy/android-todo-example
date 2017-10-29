fd1a2b3064dd74691d1e825fc4977633b242c97a
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-10-29 13:53:57 +0900

    Add method to create Intent to go AddTaskActivity

--- app/src/main/java/com/mickamy/todo/AddTaskActivity.java
+++ app/src/main/java/com/mickamy/todo/AddTaskActivity.java
@@ -1,9 +1,15 @@
 package com.mickamy.todo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class AddTaskActivity extends AppCompatActivity {
+
+    static Intent createIntent(Context context) {
+        return new Intent(context, AddTaskActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
