6868a2b6f7852a7fc1b43c9e09c2e7e09f735b5d
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-11-01 01:22:14 +0900

    Remove unused line

--- app/src/main/java/com/mickamy/todo/MainActivity.java
+++ app/src/main/java/com/mickamy/todo/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -24,9 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         configureLayout();
-
-        List<Task> tasks = TaskStorage.getInstance().getAll();
-        Log.d(TAG, "onCreate: " + tasks);
     }
 
     private void configureLayout() {
