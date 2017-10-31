00113a5450266df6f8af44686c50a23b117498a4
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-11-01 01:17:37 +0900

    Configure ListView in onResume

--- app/src/main/java/com/mickamy/todo/MainActivity.java
+++ app/src/main/java/com/mickamy/todo/MainActivity.java
@@ -7,12 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ListView listView;
+    private ArrayAdapter<Task> taskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +40,26 @@ public class MainActivity extends AppCompatActivity {
                 startAddTask();
             }
         });
+        listView = findViewById(R.id.list_task);
+        configureTaskList();
+    }
+
+    private void configureTaskList() {
+        List<Task> tasks = TaskStorage.getInstance().getAll();
+        int itemLayout = R.layout.item_task;
+        int titleId = R.id.text_title;
+        taskAdapter = new ArrayAdapter<>(this,
+                itemLayout,
+                titleId,
+                tasks
+        );
+        listView.setAdapter(taskAdapter);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        List<Task> tasks = TaskStorage.getInstance().getAll();
-        Log.d(TAG, "onResume: " + tasks);
+        configureTaskList();
     }
 
     private void startAddTask() {
